65a7e635336cbad19a7734b689912bca7e0f06a5
Marius Strumyla <marius.str@protonmail.com>
2022-04-28 22:17:28 +0300

    To do list delete function added

--- index.js
+++ index.js
@@ -31,7 +31,12 @@ window.addEventListener('load', () => {
 		task_edit_el.classList.add('edit');
 		task_edit_el.innerText = 'Edit';
 
+        const task_delete_el = document.createElement('button');
+		task_delete_el.classList.add('delete');
+		task_delete_el.innerText = 'Delete';
+
 		task_actions_el.appendChild(task_edit_el);
+        task_actions_el.appendChild(task_delete_el);
 
 		task_el.appendChild(task_actions_el);
 
@@ -48,6 +53,10 @@ window.addEventListener('load', () => {
 				task_edit_el.innerText = "Edit";
 				task_input_el.setAttribute("readonly", "readonly");
 			}
+		});
+        
+        task_delete_el.addEventListener('click', (e) => {
+			list_el.removeChild(task_el);
 		});
     })
 })
